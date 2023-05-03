b16f47ad89f315c0ad723a805251352b9dc544f4
m-diana-dev <m.diana.dev@gmail.com>
2023-05-03 12:12:00 +0300

    lesson 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import {TodoList} from "./TodoList";
+import {Todolist} from './Todolist';
+
+export type FilterBtn = 'All' | 'Active' | 'Completed';
 
 function App() {
-    const tasks1 = [
-        {id: 1, title: "HTML&CSS", isDone: true},
-        {id: 2, title: "JS", isDone: true},
-        {id: 3, title: "ReactJS", isDone: false},
-        {id: 4, title: "ReactJS2", isDone: false}
-    ]
-    const tasks2 = [
-        {id: 1, title: "Hello world", isDone: true},
-        {id: 2, title: "I am Happy", isDone: false},
-        {id: 3, title: "Yo", isDone: false}
-    ]
-    const title1 = 'Title 1';
-    const title2 = 'Title 2';
+
+    let [tasks, setTasks] = useState(
+        [
+            {id: 1, title: "HTML&CSS", isDone: true},
+            {id: 2, title: "JS", isDone: true},
+            {id: 3, title: "ReactJS", isDone: false},
+            {id: 4, title: "ReactJS", isDone: false},
+            {id: 5, title: "ReactJS", isDone: false}
+        ]
+    )
+    const RemoveTask = (id: number) => {
+        setTasks(tasks.filter(el => el.id !== id));
+    }
+
+    let [tasksAfterFilter, setTasksAfterFilter] = useState('All');
+
+    const FilteredTasks = (filterString: FilterBtn) => {
+        setTasksAfterFilter(filterString);
+    }
+
+    let filteredTasks =  tasks;
+    if(tasksAfterFilter === 'Active'){
+        filteredTasks = tasks.filter(el=>!el.isDone);
+    } else if (tasksAfterFilter === 'Completed'){
+        filteredTasks = tasks.filter(el=>el.isDone);
+    }
 
     return (
-        <>
-            <TodoList tasks={tasks1} title={title1}/>
-            <TodoList tasks={tasks2} title={title2}/>
-        </>
-)
-    ;
+        <div className="App">
+            <Todolist title="What to learn" tasks={filteredTasks} RemoveTask={RemoveTask} FilteredTasks={FilteredTasks}/>
+        </div>
+    );
 }
 
 export default App;
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,36 +1,41 @@
 import React from 'react';
+import {FilterBtn} from "./App";
 
-type TodoListPropsType = {
-    tasks: Array<TasksType>
+type TaskType = {
+    id: number
     title: string
-}
-
-type TasksType = {
-    id: number,
-    title: string,
     isDone: boolean
 }
 
-export function TodoList(props: TodoListPropsType) {
-    return (
-        <div className="App">
-            <div>
-                <h3>{props.title}</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    {props.tasks.map(el => {
-                        return (<li><input type="checkbox" checked={el.isDone}/> <span>{el.title}</span></li>)
-                    })}
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+type PropsType = {
+    title: string
+    tasks: Array<TaskType>
+    RemoveTask: (id: number)=>void
+    FilteredTasks:(filterString: FilterBtn)=>void
+}
+
+export function Todolist(props: PropsType) {
+    return <div>
+        <h3>{props.title}</h3>
+        <div>
+            <input/>
+            <button>+</button>
         </div>
-    );
+        <ul>
+            {props.tasks.map((el)=>{
+                return(
+                    <li key={el.id}>
+                        <input type="checkbox" checked={el.isDone}/>
+                        <span>{el.title}</span>
+                        <button onClick={()=>{props.RemoveTask(el.id)}}>X</button>
+                    </li>
+                )
+            })}
+        </ul>
+        <div>
+            <button onClick={()=>props.FilteredTasks('All')}>All</button>
+            <button onClick={()=>props.FilteredTasks('Active')}>Active</button>
+            <button onClick={()=>props.FilteredTasks('Completed')}>Completed</button>
+        </div>
+    </div>
 }
